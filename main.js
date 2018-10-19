7f91effb6e727a1ef7c041a97d082ca35f18da3b
Ian Chin <skyhawk3102000@yahoo.com>
2018-10-18 22:18:48 -0400

    Added Horizontal button and data

--- main.js
+++ main.js
@@ -18,14 +18,61 @@ const options = {
         1567442,
         1563025,
         1469845,
-        1394928
+        1394928,
+        1300092,
+        1026908
       ]
     }
-  ]
+  ],
+  xaxis: {
+    categories: [
+      'New York',
+      'Los Angeles',
+      'Chicago',
+      'Houston',
+      'Philadelphia',
+      'Phoenix',
+      'San Antonio',
+      'San Diego',
+      'Dallas',
+      'San Jose'
+    ]
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false
+    }
+  },
+  fill: {
+    colors: ['#f44336']
+  },
+  dataLabels: {
+    enabled: false
+  },
+  title: {
+    text: 'Largest US Cities By Population',
+    align: 'center',
+    margin: 20,
+    offsetY: 20,
+    style: {
+      fontSize: '25px'
+    }
+  }
 };
 
 // Init Chart
-const chart = new ApexCharts(document.getElementById('#chart'), options);
+const chart = new ApexCharts(document.querySelector('#chart'), options);
 
 //Render Chart
 chart.render();
+
+// Event Method Example
+document.querySelector('button').addEventListener('click', () =>
+  chart.updateOptions({
+    plotOptions: {
+      bar: {
+        horizontal: true
+      }
+    }
+  })
+);
